5b45327ed779bbb7a1f8a36f237127a734df3292
Bakhtiyor <alijanovbakhtiyor@gmail.com>
2022-05-16 01:16:02 +0500

    readme updated

--- README.md
+++ README.md
@@ -2,11 +2,9 @@
 
 > Необходимо установка Docker(https://docs.docker.com/get-docker/) 
 > и Docker Compose(https://docs.docker.com/compose/) на вашем устройстве.
----
 
 1. `git clone https://github.com/bakhtiyor-dev/soato.git`
 2. Переходим к папке <i>soato</i>, создаем файл .env и копируем из .env.example в .env
 3. `docker run --rm --interactive --tty --volume ${pwd}:/app composer install --ignore-platform-reqs` (в Windows только через Powershell),
 4. `docker compose up`
 5. перейти по localhost и нажать на установить
-```
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,15 @@
 <?php
 
 use App\Http\Controllers\InstallController;
-use App\Imports\SoatoImport;
+use App\Http\Controllers\StudentController;
 use App\Models\District;
 use App\Models\Region;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Schema;
-use Maatwebsite\Excel\Facades\Excel;
-
-Route::get('/test', function () {
-    Artisan::call('db:wipe');
-});
 
 Route::get('/install', [InstallController::class, 'install'])->name('install');
+Route::get('/', [InstallController::class, 'index']);
 
-Route::get('/', InstallController::class);
-
-Route::resource('students', \App\Http\Controllers\StudentController::class)->only(['index', 'create', 'store']);
+Route::resource('students', StudentController::class)->only(['index', 'create', 'store']);
 
 Route::get(
     '/api/regions/{code}/districts',
